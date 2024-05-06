59ddbcc20f4e715cdda0316619941b6b6adcbd32
Şeyma <sirakaya.seymaa@gmail.com>
2024-05-06 11:05:34 +0300

    junit impl

---
+++ src/main/java/org/etiya/HomepageTests.java
@@ -0,0 +1,23 @@
+package org.etiya;
+
+import org.junit.Test;
+
+public class HomepageTests
+{
+
+    @Test
+    public void test1()
+    {
+        //3A Principle
+        //Arrange => Kullanılacak verileri hazirla (Teste hazırlık)
+        //Act =>  => Yapılacak testin aksiyonunun çalıştıırlmaıs. ( testin koşulmaya başlanması.)
+        //Assert  => Testin sonucu ile beklenen sonuç arasındaki karşılaştırma.
+    }
+
+    @Test
+    public void test2()
+    {
+
+    }
+
+}
